9036d9b6acc64d5dfab12628820973f9afff249e
OkoTzy <113184680+OkoTzy@users.noreply.github.com>
2022-09-09 13:42:02 +0000

    Add files via upload

---
+++ Object Oriented Programming 1/ProgrammingExercise1.4/ProgrammingExercise1.4/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub PrintButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PrintButton.Click
+
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+
+    End Sub
+    Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub FirstButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FirstButton.Click
+
+        First_DL.Text = "Police Dept."
+        F_Phone.Text = "(602)554 - 322"
+
+    End Sub
+
+    Private Sub SecondButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SecondButton.Click
+
+        Second_DL.Text = "Ambulance"
+        S_Phone.Text = "(602) 554 - 765"
+
+    End Sub
+
+    Private Sub ThirdButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ThirdButton.Click
+
+        Third_DL.Text = "Civil Protection"
+        T_Phone.Text = "(602) 554 - 991"
+
+    End Sub
+
+    Private Sub FourthButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FourthButton.Click
+
+        Fourth_DL.Text = "Hospital"
+        L_Phone.Text = "(602) 554 - 371"
+
+    End Sub
+End Class
---
+++ Object Oriented Programming 1/ProgrammingExercise1.5/ProgrammingExercise1.5/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+
+    Private Sub DinerName_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DinerName.Click
+
+    End Sub
+
+    Private Sub ExitButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitButton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub PrintButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PrintButton.Click
+
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+
+    End Sub
+
+    Private Sub SoupButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SoupButton.Click
+
+        MessageLabel.Text = "Tikka Masala Soup"
+
+    End Sub
+
+    Private Sub SpecialButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SpecialButton.Click
+
+        MessageLabel.Text = "Creamy Presto Shrimp"
+
+    End Sub
+
+    Private Sub DailyButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles DailyButton.Click
+
+        MessageLabel.Text = "Pan-Seared Salmon"
+
+    End Sub
+
+    Private Sub MessageLabel_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MessageLabel.Click
+
+    End Sub
+End Class
